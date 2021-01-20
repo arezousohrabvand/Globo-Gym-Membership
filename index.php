242bb8b1a8b99c3b29a4db2e434ba89d8c032271
Arezou Sohrabvand <200387480@student.georgianc.on.ca>
2021-01-20 12:31:35 -0500

    upload web app

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php
+    //set default value of variables for initial page load
+    if (!isset($name)) { $name = ''; }
+    if (!isset($password)) {$password = ''; }
+    if (!isset($email)) { $email = ''; }
+    if (!isset($age)) { $age = ''; }
+    if (!isset($membership_lenght)) { $membership_lenght= ''; }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignment2</title>
+      <link href="css/styles.css" rel="stylesheet">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <main>
+    <h1>Globo-Gym Membership</h1>
+    <h2>Sign-Up Information</h2>
+    <img src="img/image.png" alt="gym" style="width:150px">
+
+    <form action="results.php" method="post">
+
+        <div id="data">
+            <label>Name</label>
+            <?php
+            //This code checks to see if we got an error message from the results.php page
+             if (!empty($error_name)) { ?>
+                <p class="error" ><?php echo htmlspecialchars($error_name); ?></p>
+            <?php } ?>
+            <input type="text" name="name"
+                   value="<?php echo htmlspecialchars($name); ?>">
+            <br>
+
+            <!-- This is where you should put the password -->
+            <label>Password:</label>
+            <?php
+            //This code checks to see if we got an error message from the results.php page
+             if (!empty($error_pass)) { ?>
+                <p class="error"><?php echo htmlspecialchars($error_pass); ?></p>
+            <?php } ?>
+            <input type="text" name="password"
+                   value="<?php echo htmlspecialchars($password); ?>">
+                   <br>
+
+            <!-- This is where you should put the email-->
+            <label>Email:</label>
+            <?php
+            //This code checks to see if we got an error message from the results.php page
+             if (!empty($error_email)) { ?>
+                <p class="error"><?php echo htmlspecialchars($error_email); ?></p>
+            <?php } ?>
+            <input type="text" name="email"
+                   value="<?php echo htmlspecialchars($email); ?>">
+                   <br>
+            <!-- This is where you should put the age-->
+            <label>Age:</label>
+            <?php
+            //This code checks to see if we got an error message from the results.php page
+             if (!empty($error_age)) { ?>
+                <p class="error"><?php echo htmlspecialchars($error_age); ?></p>
+            <?php } ?>
+            <input type="text" name="age"
+                    value="<?php echo htmlspecialchars($age); ?>">
+                    <br>
+
+            <input type="checkbox" name="facility" value="Locker"> Require Locker<br>
+
+            <?php
+            //This code checks to see if we got an error message from the results.php page
+             if (!empty($error_membership)) { ?>
+                <p  class="error"><?php echo htmlspecialchars($error_membership); ?></p>
+            <?php } ?>
+
+            <select name="membership_lenght">
+              <option selected="" value="Default">(Please select your membership lenght)</option>
+              <option value="1month" >1Month-$30</option>
+              <option value="1year" >1Year-$300</option>
+              <option value="lifetime" >Lifetime-$3000</option>
+              </select>
+              <br>
+
+              <div class="slidecontainer">
+                <p>Current fitness level:</p>
+                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+                  <p>Current Fitness: <span id="demo"></span></p>
+                  <div id="buttons">
+                      <label>&nbsp;</label>
+                      <input type="submit" value="Submit"><br>
+                  </div>
+              </div>
+
+              <script>
+                  var slider = document.getElementById("myRange");
+                  var output = document.getElementById("demo");
+                  output.innerHTML = slider.value;
+
+                  slider.oninput = function() {
+                    output.innerHTML = this.value;
+                  }
+              </script>
+
+        </div>
+
+
+
+
+    </form>
+    </main>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,88 @@
+<?php
+    // get the data from the form
+
+    $name = filter_input(INPUT_POST, 'name');
+        $namelength=strlen($name);
+
+    $password = filter_input(INPUT_POST, 'password');
+        $passwordlength=strlen($password);
+
+    $email= filter_input(INPUT_POST, 'email',
+        FILTER_VALIDATE_EMAIL);
+
+    $age= filter_input(INPUT_POST, 'age',
+        FILTER_VALIDATE_INT);
+
+
+    // validate Globo-Gym Membership
+    // Here is where you should validate name
+$error_name = $error_pass = $error_email = $error_age = $error_membership = $error_message = "";
+
+if ( strlen($name)<5 )
+{
+        $error_name = 'Username should be at least 5 characters in lenght.';
+        $error_message = '1';
+    }
+
+    // Here is where you should validate password
+    if ( strlen($password)<6) {
+        $error_pass = 'Password should be at least 5 characters in lenght.';
+        $error_message='1';
+
+    }
+    // Here is where you should validate email
+    if ($email ===FALSE) {
+      $error_email = 'Your email is not valid.';
+      $error_message='1';
+
+    }
+
+    // Here is where you should validate age
+   if ($age===FALSE) {
+    $error_age = 'Your age must be a whole number.';
+    $error_message='1';
+  } else if ( $age <= 18) {
+    $error_age = 'Age must be greater than 18.';
+    $error_message='1';
+  } else if ( $age >100) {
+    $error_age = 'Age must be less than 101.';
+    $error_message='1';
+  }
+
+  if (!isset($membership_lenght)) {
+    $error_membership = "Please choose a membership plan";
+  }
+
+    // if an error message exists, go to the index page
+    if ($error_message != '') {
+        //This redirects us to the index.php page again and displays the error_message
+        include('index.php');
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignment2</title>
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <h1>Successful Application</h1>
+
+        <img src="img/image2.png" alt="welcome" style="width:180px"><br>
+
+        <label> Welcome</label>
+        <span><?php echo  $name; ?></span><label>!</label><br>
+
+        <label>You email address is </label>
+        <span><?php echo $email; ?></span><br>
+
+        <label>We think you will be fit at age</label>
+
+        <span><?php echo $age; ?></span><br>
+
+    </main>
+</body>
+</html>
